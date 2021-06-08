6730422e96178c766c765c4b7284ff2c9e896f74
Jyotshna-intern <84559003+Jyotshna-intern@users.noreply.github.com>
2021-06-08 20:00:59 +0530

    Update Solid_principles.java

--- Solid_principles.java
+++ Solid_principles.java
@@ -9,35 +9,30 @@ interface Classes
 
 public class Solid_principles {
 	public static void main(String[] args) {
-		
-		Teacher teacher=new Teacher();
-		//teacher register
-		teacher.register(teacher);
 		Attendance attendance=new Attendance();
 		BiometricReader biometricReader=new BiometricReader();
-		//teacher login
+		
+		Teacher teacher=new Teacher();
+		
+		teacher.register(teacher);
+		
 		teacher.checkin(attendance,biometricReader);
 		
-		//attend class
 		teacher.attendClasses();
 		
-		//correct papers and assign marks
 		teacher.correctPapers();
 		teacher.assignMarks();
 		
 		
 		Student student =new Student();
 		
-		//student registration
 		student.register(student);
 		
-		//student login
+		
 		student.checkin(attendance,biometricReader);
 		
-		//attend classes
 		student.attendClasses();
 		
-		//write exams and view marks
 		student.writeExam();
 		student.viewMarks();
 		
